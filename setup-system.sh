1c908e556f4beff6fa5f4c015cff98114e5d5388
Matthew Trentacoste <web+git@matttrent.com>
2017-06-13 22:12:28 -0700

    fixing setup script

--- setup-system.sh
+++ setup-system.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-cd $HOME
+sudo apt-get update
 
-git clone https://github.com/matttrent/dotfiles.git
-cd dotfiles
-bash install.sh
-bash aws-set-zsh.sh
+if [ ! -e $HOME/dotfiles ]; then
+    cd $HOME
+    git clone https://github.com/matttrent/dotfiles.git
+    cd dotfiles
+    bash install.sh
+    echo "installed dotfiles"
+fi
+
+if [[ ! $SHELL =~ 'zsh' ]]; then
+    sudo apt-get -y install zsh
+    sudo chsh -s $(which zsh) ubuntu
+    echo "shell changed to zsh"
+fi
+
+cd $HOME
