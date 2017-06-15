d18211a89abb6566b9e7c93d46099910f7a9d8c4
Matthew Trentacoste <web+git@matttrent.com>
2017-06-14 18:32:03 -0700

    cleanup

--- setup-system.sh
+++ setup-system.sh
@@ -2,18 +2,18 @@
 
 sudo apt-get update
 
+if [[ ! $SHELL =~ 'zsh' ]]; then
+    sudo apt-get -y install zsh
+    sudo chsh -s $(which zsh) ubuntu
+    echo "shell changed to zsh"
+fi
+
 if [ ! -e $HOME/dotfiles ]; then
     cd $HOME
     git clone https://github.com/matttrent/dotfiles.git
     cd dotfiles
     bash install.sh
     echo "installed dotfiles"
-fi
-
-if [[ ! $SHELL =~ 'zsh' ]]; then
-    sudo apt-get -y install zsh
-    sudo chsh -s $(which zsh) ubuntu
-    echo "shell changed to zsh"
 fi
 
 cd $HOME
